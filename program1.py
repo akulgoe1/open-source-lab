5474246397cc0594422085cb70e833b8c8456b98
akulgoe1 <110814342+akulgoe1@users.noreply.github.com>
2022-08-08 14:00:42 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,16 @@
+  # WAP to count frequency of an element in an array
+arr = [1, 2, 8, 3, 2, 2, 2, 5, 1];     
+fr = [None] * len(arr);    
+visited = -1;    
+for i in range(0, len(arr)):    
+    count = 1;    
+    for j in range(i+1, len(arr)):    
+        if(arr[i] == arr[j]):    
+            count = count + 1;      
+            fr[j] = visited;   
+    if(fr[i] != visited):    
+        fr[i] = count;      
+print(" Element | Frequency");       
+for i in range(0, len(fr)):    
+    if(fr[i] != visited):    
+        print("    " + str(arr[i]) + "    |    " + str(fr[i]));    
